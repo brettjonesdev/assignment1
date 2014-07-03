73e0db807cb37225747030c6a69f5a3ca36d9e37
Brett Jones <brettjonesdev@gmail.com>
2014-07-03 15:09:10 -0700

    assignment 2

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,17 +1,46 @@
 import sys
+import re
+import json
 
-def hw():
-    print 'Hello, world!'
+def get_dictionary(file):
+    dictionary = {}
+    for line in file:
+        term, score = line.split("\t")
+        dictionary[term] = int(score)
 
-def lines(fp):
-    print str(len(fp.readlines()))
+    return dictionary
+
+
+def get_sentiment_score(tweet, dictionary):
+    score = 0
+    terms = re.findall(r"\w+", tweet)
+    for term in terms:
+        if term in dictionary:
+            score += dictionary[term]
+
+    return score
+
+
+def get_tweet_text_from_file(file):
+    texts = []
+    for line in file:
+        obj = json.loads(line)
+        if 'text' in obj:
+            texts.append(obj['text'])
+
+    return texts
+
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    sentiment_dictionary = get_dictionary(sent_file)
+
+    tweets = get_tweet_text_from_file(tweet_file)
+
+    for tweet in tweets:
+        print get_sentiment_score(tweet, sentiment_dictionary)
 
 if __name__ == '__main__':
     main()
